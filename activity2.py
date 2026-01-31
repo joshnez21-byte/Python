25a029d8aec3b09944790bada12065598a6561e9
joshnez21-byte <joshnez21@gmail.com>
2026-01-31 19:29:55 +0300

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,15 @@
+first_name = "Codingal" 
+last_name = "Education"
+full_name = first_name + last_name
+example = "Haa"*3
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("Example String:", example)
+
+word = "Coding"
+print("Length of the String:",len(word))
+print("First letter of string:",word[0])
+print("Last letter of string:",word[5])
+print("String sliced:",word[0:3])
