415e73dfd115f11bea7a2d7e2888b7b49ae0e000
joshnez21-byte <joshnez21@gmail.com>
2026-02-01 18:37:20 +0300

    Add files via upload

--- activity.py
+++ activity.py
@@ -1,21 +1,7 @@
-name = "Joshua"
-age = 16
-is_student = True
-weight = 69.5
+number = int(input("Enter number to check: "))
+print("Number to be checked:",number)
 
-print("Name:", name)
-print("Data type of Name is",type(name))
-print("Age:", age)
-print("Data Type of Age is",type(age))
-print("Is Student:", is_student)
-print("Data Type of Is Student is",type(is_student))
-print("Weight:", weight)
-print("Data Type of Weight is",type(weight))
-
-print("\n After Type Casting...")
-age = str(age)
-print(age)
-print("Data type of Age is",type(age))
-weight = int(weight)
-print(weight)
-print("Data Type of Weight is",type(weight))
+if number%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
