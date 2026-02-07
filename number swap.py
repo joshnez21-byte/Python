f3039c27d43a403c6ef7bed0727379410719c922
joshnez21-byte <joshnez21@gmail.com>
2026-02-07 13:10:32 +0300

    Add files via upload

---
+++ number swap.py
@@ -0,0 +1,19 @@
+# Take input values from user
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+z = input("Enter Value of z:")
+
+# Swapping
+temp = x
+x = y
+y = temp
+
+# Swapping z with x
+temp = x
+x = z
+z = temp
+
+# Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
+print("value of z after swapping", z)
